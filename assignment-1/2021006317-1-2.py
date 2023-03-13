d8a9efa9e363a651b3d372f7bdf6112c8a8461cd
2021006317 <eve7777@hanyang.ac.kr>
2023-03-13 10:53:06 +0900

    week1

--- assignment-1/2021006317-1-2.py
+++ assignment-1/2021006317-1-2.py
@@ -1,23 +1,18 @@
 import numpy as np
 
-print('A')
 M = np.array([2+i for i in range(25)])
 print(M)
 
-print('B')
 M = M.reshape(5,5)
 print(M)
 
-print('C')
 for i in range(5):
     M[i][0] = 0
 print(M)
 
-print('D')
 M = M*M
 print(M)
 
-print('E')
 mv=0
 for i in range(5):
     mv += M[0][i]
