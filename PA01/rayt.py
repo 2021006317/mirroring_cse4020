ebcf260c80db3fc88f652118940999512527a139
2021006317 <eve7777@hanyang.ac.kr>
2023-04-14 15:31:52 +0900

    four

--- PA01/rayt.py
+++ PA01/rayt.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*
-# sample_python aims to allow seamless integration with lua.
-# see examples below
-
 import os
 import sys
 import pdb  # use pdb.set_trace() for debugging
@@ -29,19 +24,20 @@ tree = ET.parse(sys.argv[1])
 root = tree.getroot()
 
 # set default values
+## camera
 viewPoint=np.array([0,0,0]).astype(np.float64)
-viewDir=np.array([0,0,-1]).astype(np.float64) # direction toward object
-viewUp=np.array([0,1,0]).astype(np.float64) # orientation of image
-viewProjNormal=-1*viewDir  # you can safely assume this. (no examples will use shifted perspective camera) # projection plane 의 normal vector(negating has no effect. default, view direction 과 동일)
-viewWidth=1.0 # dimensions of viewing window on the image plane
-viewHeight=1.0 # "
-projDistance=1.0 # d. image 사각형 ~ projection center distance. 근데? projection center 이 viewpoint 라고 하네요.
-
-position=np.array([0,0,0]).astype(np.float64)
+viewDir=np.array([0,0,-1]).astype(np.float64)
+viewUp=np.array([0,1,0]).astype(np.float64)
+viewProjNormal=-1*viewDir
+viewWidth=1.0
+viewHeight=1.0
+projDistance=1.0
+## light
+light_position=np.array([0,0,0]).astype(np.float64)
 intensity=np.array([1,1,1]).astype(np.float64)  # how bright the light is.
-
+## shading
 exponent = 0
-
+## spehre
 center = np.array([0,0,0]).astype(np.float64)
 radius = 0
 color = "blue"
@@ -52,16 +48,14 @@ def unit_dir(dir):
         udir = t*dir
         return udir
 
-# diffusely reflected light
+# lambertian shading
 def lamb_shading(kd, I, n, l):
     m = max(0, np.dot(n,l))
     ldm = np.array([kd[0] * I[0], kd[1] * I[1], kd[2] * I[2]])
     ld = ldm*m
-    # print(f'kd is {kd}')
-    # print(f'ld is {ld}')
     return ld
     
-# specularly reflected light
+# blinn_phong shading
 def phong_shading(ks, I, v, l, n):
     h = (v+l)/np.sqrt(np.dot(v+l,v+l))
     m = max(0,np.dot(n,h))**exponent
@@ -114,20 +108,17 @@ def main():
     print(f'surface is {surface}')
 
     for c in root.findall('light'):
-        position = np.array(c.findtext('position').split()).astype(np.float64)
+        light_position = np.array(c.findtext('position').split()).astype(np.float64)
         intensity = np.array(c.findtext('intensity').split()).astype(np.float64)
-    print(f'light position is {position} and intensity is {intensity}')
+    print(f'light_position is {light_position} and intensity is {intensity}')
 
-    # camera frame
+    # camera frame coordination
     vec_u = unit_dir(np.cross(viewDir, viewUp))
     vec_w = (-1)*unit_dir(viewDir)
     vec_v = unit_dir(np.cross(vec_w, vec_u))
     print(f'(u,v,w) is {(vec_u, vec_v, vec_w)}')
 
-    # world frame through viewPoint e
-    # u = l + (r-l)*(i+0.5)/n_x
-    # v = b + (t-b)*(j+0.5)/n_y
-    # 근데 우리 그림은 왼쪽 위가 (0,0)이니까 양수b-(t-b)...
+    # world frame coordination through viewPoint p on image plane
     def vector_os(i, j):
         coord_u = (-1)*viewWidth/2 + viewWidth*(i+0.5)/imgSize[0]
         coord_v = viewHeight/2  - viewHeight*(j+0.5)/imgSize[1]
@@ -135,7 +126,7 @@ def main():
         return s
     
     # |p+td-c|=radius
-    # p+td = ray, c = sphere center 
+    # p+td = ray, c = sphere center
     def intersect_distance(p, unit_d, sphere_center, sphere_radius):
         cp_point = p - sphere_center # OP - OC = CP
         discriminant = (np.dot(unit_d, cp_point))**2 - np.dot(cp_point,cp_point) + sphere_radius**2
@@ -151,10 +142,8 @@ def main():
             elif t2>=0:
                 return t2
             else:
-                # 뒤에서 만남. 근데 이게 됨?
                 return 99999999
         else:
-            # 안 만남
             return 99999999
 
     def find_min(d_list):
@@ -167,105 +156,65 @@ def main():
                 idx = i
         return (idx, min)
 
-    def shadow(light_p, unit_d, sphere_center, sphere_radius):
-        lc_point = light_p - sphere_center
-        discriminant = (np.dot(unit_d, lc_point))**2 - np.dot(lc_point,lc_point) + sphere_radius**2
-
-        if discriminant >=0:
-            t1 = -np.dot(unit_d, lc_point) - np.sqrt(discriminant)
-            t2 = -np.dot(unit_d, lc_point) + np.sqrt(discriminant)
-            if t1>=0 or t2>=0: # intersected, not blocked -> not shadow
-                return 0
-            else: # not intersected || intersected but blocked -> shadow
-                return 1
-        else:
-            return 1
-
     # var window = image plane 의 window center
     window = viewPoint + unit_dir(viewDir)
     print(f'window ray is {window}')
 
     # Create an empty image
     channels=3
-    img = np.zeros((imgSize[1], imgSize[0], channels), dtype=np.uint8) # 0으로 된 3x300 행렬 300개를 담은 리스트
+    img = np.zeros((imgSize[1], imgSize[0], channels), dtype=np.uint8)
     img[:,:]=0
 
     # main rendering
-    print(vector_os(0,0))
     for i in np.arange(imgSize[0]):
         for j in np.arange(imgSize[1]):
             img[j][i] = Color(0,0,0).toUINT8()
             vec_ij = vector_os(i,j)
             view_distance = []
-            # surface = [(color, center, radius, shade_typ), (), ...]
+            light_distance = []
+
+            # 눈에 보이는 도형 찾기
             for sphere in surface:
-                # 거리를 리스트에 넣는다.
-                dir_pi = vec_ij - viewPoint
-                vd = intersect_distance(viewPoint, unit_dir(dir_pi), sphere[1], sphere[2])
+                dir_ps = unit_dir(vec_ij - viewPoint) # OS - OP = PS
+                vd = intersect_distance(viewPoint, dir_ps, sphere[1], sphere[2])
                 view_distance.append(vd)
-            # 최소 거리를 구한다.
             vidx, vmin = find_min(view_distance)
 
-            prime_s = viewPoint + vmin*unit_dir(dir_pi)
-            dir_ls = prime_s - position
+            # 빛 받는 도형 찾기
+            for sphere in surface:
+                vec_ot = viewPoint + vmin*dir_ps # image plane 너머의 공간 상의 점
+                dir_lt = unit_dir(vec_ot - light_position) # OT - OL = LT
+                ld = intersect_distance(light_position, dir_lt, sphere[1], sphere[2])
+                light_distance.append(ld)
+            lidx, lmin = find_min(light_distance)
+                
+            # 보이는 구가 없는 경우
+            if vmin == 99999999:
+                img[j][i] = Color(0,0,0).toUINT8()
             
-            # 내눈에 보인다.
-            if vmin!=99999999:
+            # 보이는 구가 있는 경우
+            else:
                 sphere = surface[vidx]
                 color = sphere[0]
                 center = sphere[1]
                 radius = sphere[2]
                 typ = sphere[3]
-                vec_cs = unit_dir(prime_s - center)
-                vec_sl = unit_dir(position - prime_s)
-                gam_color = Color(color[0], color[1], color[2])
-                gam_color.gammaCorrect(2.2)
-                img[j][i] = gam_color.toUINT8()
+                dir_ct = unit_dir(vec_ot - center)
+                dir_tl = unit_dir(light_position - vec_ot)
 
-                # 빛이랑 닿는지 확인.
-                ld = intersect_distance(position, unit_dir(dir_ls), center, radius)
-
-                # 빛이랑 닿는다.
-                if ld != 99999999:
-                    is_shadow = shadow(position, unit_dir(dir_ls), center, radius)
-                    if is_shadow==0:
-                        if typ == "Lambertian" :
-                            clr = lamb_shading(color, intensity, unit_dir(vec_cs), unit_dir(vec_sl))
-                            gam_color = Color(clr[0], clr[1], clr[2])
-                            gam_color.gammaCorrect(2.2)
-                            img[j][i] = gam_color.toUINT8()
-                        elif typ == "Phong":
-                            pass
-                    elif is_shadow==1:
-                        img[j][i] = Color(0,0,0).toUINT8()
-                # view 최소거리 && light 최소거리 -> shading. type 확인후 알잘 함수. kd+ld
+                # 보이는 구가 빛을 받는 경우
+                if vidx == lidx:
+                    if typ == "Lambertian" :
+                        clr = lamb_shading(color, intensity, dir_ct, dir_tl)
+                        gam_color = Color(clr[0], clr[1], clr[2])
+                        gam_color.gammaCorrect(2.2)
+                        img[j][i] = gam_color.toUINT8()
+                    elif typ == "Phong":
+                        pass
                 
-                # 빛이랑 안닿음.
-                else:
-                    is_shadow = shadow(position, unit_dir(vec_sl), center, radius)
-                    # if is_shadow == 1:
+                # 보이는 구가 빛을 받지 못하는 경우
+                elif vidx != lidx:
                     img[j][i] = Color(0,0,0).toUINT8()
-
-                '''
-                # 빛이 닿음.
-                if cnt!=0:
-                    if vidx == lidx:
-                        if typ == "Lambertian" :
-                            clr = lamb_shading(color, intensity, unit_dir(vec_cs), unit_dir(vec_sl))
-                            img[j][i] = Color(clr[0], clr[1], clr[2]).toUINT8()
-                        elif typ == "Phong":
-                            pass
-                '''
-                # view 최소거리 but light 최소x -> shadow 오. 오! -> 이게 빛이 안닿는데 내눈에 보이는거잖아
-                
-
-            # 내눈에 안보임.
-            else:                
-                img[j][i] = Color(0,0,0).toUINT8()
-
-    # white = Color(1,1,1).toUINT8()
-    # for i in range(300):
-    #     img[0][i] = white
 
     rawimg = Image.fromarray(img, 'RGB')
     rawimg.save(sys.argv[1]+'.png')
